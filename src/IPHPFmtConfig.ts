5520dec317d553ec7a0c8ac985e1640c37d70eae
Filip Rak <filiprak@wp.pl>
2022-02-20 16:20:16 +0100

    Cleanup

--- src/IPHPFmtConfig.ts
+++ src/IPHPFmtConfig.ts
@@ -1,16 +1,5 @@
 export default interface IPHPFmtConfig {
-  php_bin: string;
-  detect_indent: boolean;
-  psr1: boolean;
-  psr1_naming: boolean;
-  psr2: boolean;
-  indent_with_space: number | boolean;
-  enable_auto_align: boolean;
-  visibility_order: boolean;
-  passes: Array<string>;
-  exclude: Array<string>;
-  smart_linebreak_after_curly: boolean;
-  yoda: boolean;
-  cakephp: boolean;
+  java_bin: string;
+  config: object,
   custom_arguments: string;
 };
--- src/PHPFmt.ts
+++ src/PHPFmt.ts
@@ -7,7 +7,6 @@ import path from 'path';
 import fs from 'fs';
 import os from 'os';
 import { execSync } from 'child_process';
-import detectIndent from 'detect-indent';
 import findUp from 'find-up';
 import IPHPFmtConfig from './IPHPFmtConfig';
 import Widget from './Widget';
@@ -34,57 +33,6 @@ class PHPFmt {
       this.args.push(this.config.custom_arguments);
       return;
     }
-
-    if (this.config.psr1) {
-      this.args.push('--psr1');
-    }
-
-    if (this.config.psr1_naming) {
-      this.args.push('--psr1-naming');
-    }
-
-    if (this.config.psr2) {
-      this.args.push('--psr2');
-    }
-
-    if (!this.config.detect_indent) {
-      const spaces: number | boolean = this.config.indent_with_space;
-      if (spaces === true) {
-        this.args.push('--indent_with_space');
-      } else if (spaces > 0) {
-        this.args.push(`--indent_with_space=${spaces}`);
-      }
-    }
-
-    if (this.config.enable_auto_align) {
-      this.args.push('--enable_auto_align');
-    }
-
-    if (this.config.visibility_order) {
-      this.args.push('--visibility_order');
-    }
-
-    const passes: Array<string> = this.config.passes;
-    if (passes.length > 0) {
-      this.args.push(`--passes=${passes.join(',')}`);
-    }
-
-    const exclude: Array<string> = this.config.exclude;
-    if (exclude.length > 0) {
-      this.args.push(`--exclude=${exclude.join(',')}`);
-    }
-
-    if (this.config.smart_linebreak_after_curly) {
-      this.args.push('--smart_linebreak_after_curly');
-    }
-
-    if (this.config.yoda) {
-      this.args.push('--yoda');
-    }
-
-    if (this.config.cakephp) {
-      this.args.push('--cakephp');
-    }
   }
 
   public getWidget(): Widget {
@@ -103,33 +51,17 @@ class PHPFmt {
 
   public format(text: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-      if (this.config.detect_indent) {
-        const indentInfo = detectIndent(text);
-        if (!indentInfo.type) {
-          // fallback to default
-          this.args.push('--indent_with_space');
-        } else if (indentInfo.type === 'space') {
-          this.args.push(`--indent_with_space=${indentInfo.amount}`);
-        }
-      } else {
-        if (this.config.indent_with_space !== 4 && this.config.psr2) {
-          return reject(
-            new Error(
-              'nbtools: For PSR2, code MUST use 4 spaces for indenting, not tabs.'
-            )
-          );
-        }
-      }
-
       let iniPath: string | undefined;
+
       const execOptions = { cwd: '' };
+
       if (Window.activeTextEditor) {
         execOptions.cwd = path.dirname(
           Window.activeTextEditor.document.fileName
         );
 
-        const workspaceFolders: WorkspaceFolder[] | undefined =
-          Workspace.workspaceFolders;
+        const workspaceFolders: WorkspaceFolder[] | undefined = Workspace.workspaceFolders;
+
         if (workspaceFolders) {
           iniPath = findUp.sync('.nbtools.ini', {
             cwd: execOptions.cwd
@@ -150,16 +82,14 @@ class PHPFmt {
       }
 
       try {
-        const stdout: Buffer = execSync(
-          `${this.config.php_bin} -r "echo PHP_VERSION_ID;"`,
-          execOptions
-        );
+        const stdout: Buffer = execSync(`${this.config.java_bin} -version`);
+
         if (Number(stdout.toString()) < 50600 && Number(stdout.toString()) > 80000) {
           return reject(new Error('nbtools: PHP version < 5.6 or > 8.0'));
         }
       } catch (err) {
         return reject(
-          new Error(`nbtools: php_bin "${this.config.php_bin}" is invalid`)
+          new Error(`nbtools: java_bin "${this.config.java_bin}" is invalid`)
         );
       }
 
@@ -174,6 +104,7 @@ class PHPFmt {
 
       try {
         fs.writeFileSync(tmpFileName, text);
+
       } catch (err) {
         this.widget.addToOutput(err.message);
         return reject(
@@ -182,33 +113,33 @@ class PHPFmt {
       }
 
       // test whether the php file has syntax error
-      try {
-        execSync(`${this.config.php_bin} -l ${tmpFileName}`, execOptions);
-      } catch (err) {
-        this.widget.addToOutput(err.message);
-        Window.setStatusBarMessage(
-          'nbtools: Format failed - syntax errors found',
-          4500
-        );
-        return reject();
-      }
+      // try {
+      //   execSync(`${this.config.java_bin} -l ${tmpFileName}`, execOptions);
+      // } catch (err) {
+      //   this.widget.addToOutput(err.message);
+      //   Window.setStatusBarMessage(
+      //     'nbtools: Format failed - syntax errors found',
+      //     4500
+      //   );
+      //   return reject();
+      // }
 
       const args: Array<string> = this.getArgs(tmpFileName);
       args.unshift(`"${PHPFmt.getJarPath()}"`);
 
       let formatCmd: string;
+
       if (!iniPath) {
-        formatCmd = `${this.config.php_bin} ${args.join(' ')}`;
+        formatCmd = `${this.config.java_bin} -jar ${args.join(' ')}`;
       } else {
-        formatCmd = `${this.config.php_bin} ${
-          args[0]
-        } --config=${iniPath} ${args.pop()}`;
+        formatCmd = `${this.config.java_bin} -jar ${args.join(' ')} --config=${iniPath}`;
       }
 
       this.widget.addToOutput(formatCmd);
 
       try {
         execSync(formatCmd, execOptions);
+
       } catch (err) {
         this.widget.addToOutput(err.message).show();
         return reject(new Error('nbtools: Execute nbtools cli failed'));
@@ -217,7 +148,7 @@ class PHPFmt {
       const formatted: string = fs.readFileSync(tmpFileName, 'utf-8');
       try {
         fs.unlinkSync(tmpFileName);
-      } catch (err) {}
+      } catch (err) { }
 
       if (formatted.length > 0) {
         resolve(formatted);
