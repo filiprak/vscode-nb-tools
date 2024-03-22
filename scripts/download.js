21269b1ace2380101b867ece53471107d336aabe
Filip Rak <filip.rak@ikol.com>
2024-03-22 16:22:58 +0100

    Cleanup

--- scripts/download.js
+++ scripts/download.js
@@ -5,7 +5,7 @@ const dest = "bin/nb-fmt.jar";
 
 request
     .get({
-        url: "https://github.com/filiprak/netbeans/releases/download/11.3-nb-fmt-1.0.0/nb-fmt.jar",
+        url: "https://github.com/filiprak/netbeans/releases/download/11.3-nb-fmt-1.0.1/nb-fmt.jar",
         followAllRedirects: true,
     })
     .on('error', function (err) {
